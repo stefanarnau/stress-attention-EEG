cb2a4c82d639bf55bcbdf37cfdd8b20ac00ce4c5
plkn <fischmechanik@gmail.com>
2022-02-11 13:17:50 +0100

    update

--- stress_attention_lateralization_2.m
+++ stress_attention_lateralization_2.m
@@ -169,6 +169,10 @@ end % End part1
 % ============================ Part 2: stats ============================================================================
 if ismember('part2a', to_execute)
 
+    % Init eeglab
+    addpath(PATH_EEGLAB);
+    eeglab;
+
     % Load chanlocs
     EEG = pop_loadset('filename', [num2str(subject_list(1)) '_autocleaned.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'info');
 
@@ -177,302 +181,138 @@ if ismember('part2a', to_execute)
     tf_freqs = dlmread([PATH_TFDECOMP 'tf_freqs.csv']);
 
     % Load ERSPs
+    load([PATH_TFDECOMP 'atra_cold_ipsi']);
+    load([PATH_TFDECOMP 'atra_cold_contra']);
+    load([PATH_TFDECOMP 'atra_warm_ipsi']);
+    load([PATH_TFDECOMP 'atra_warm_contra']);
     load([PATH_TFDECOMP 'atra_z_cold_ipsi']);
     load([PATH_TFDECOMP 'atra_z_cold_contra']);
     load([PATH_TFDECOMP 'atra_z_warm_ipsi']);
     load([PATH_TFDECOMP 'atra_z_warm_contra']);
 
 
-    cluster_channels = [18, 21, 23, 27, 24, 26]
 
+    % Define channels in cluster
+    cluster_channels = [18, 21, 23, 27, 24, 26];
+
+    % Build matrices for ANOVA
+    anova_matrix = [];
+    for s = 1 : size(atra_cold_ipsi, 1)
+        tidx = tf_times >= -500 & tf_times < -200;
+        anova_matrix(s, 1, 1) = mean2(atra_cold_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 1, 2) = mean2(atra_cold_contra(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 1) = mean2(atra_warm_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 2) = mean2(atra_warm_contra(s, cluster_channels, tidx));
+    end
+    t = simple_mixed_anova(anova_matrix, [], {'stress', 'hemisqhere'});
+
+    % Build matrices for ANOVA
+    anova_matrix = [];
+    for s = 1 : size(atra_cold_ipsi, 1)
+        tidx = tf_times >= -500 & tf_times < -200;
+        anova_matrix(s, 1, 1) = mean2(atra_z_cold_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 1, 2) = mean2(atra_z_cold_contra(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 1) = mean2(atra_z_warm_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 2) = mean2(atra_z_warm_contra(s, cluster_channels, tidx));
+    end
+    t_z = simple_mixed_anova(anova_matrix, [], {'stress', 'hemisqhere'});
+
+    % Build matrices for ANOVA
+    anova_matrix = [];
+    for s = 1 : size(atra_cold_ipsi, 1)
+        tidx = tf_times >= 900 & tf_times < 1500;
+        anova_matrix(s, 1, 1) = mean2(atra_cold_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 1, 2) = mean2(atra_cold_contra(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 1) = mean2(atra_warm_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 2) = mean2(atra_warm_contra(s, cluster_channels, tidx));
+    end
+    t_post = simple_mixed_anova(anova_matrix, [], {'stress', 'hemisqhere'});
+
+    % Build matrices for ANOVA
+    anova_matrix = [];
+    for s = 1 : size(atra_cold_ipsi, 1)
+        tidx = tf_times >= 900 & tf_times < 1500;
+        anova_matrix(s, 1, 1) = mean2(atra_z_cold_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 1, 2) = mean2(atra_z_cold_contra(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 1) = mean2(atra_z_warm_ipsi(s, cluster_channels, tidx));
+        anova_matrix(s, 2, 2) = mean2(atra_z_warm_contra(s, cluster_channels, tidx));
+    end
+    t_post_z = simple_mixed_anova(anova_matrix, [], {'stress', 'hemisqhere'});
+
+    % Build matrices for ANOVA
+    anova_matrix = [];
+    for s = 1 : size(atra_cold_ipsi, 1)
+        tidx = tf_times >= -500 & tf_times < 0;
+        anova_matrix(s, 1) = (mean2(atra_cold_ipsi(s, cluster_channels, tidx)) + mean2(atra_cold_contra(s, cluster_channels, tidx))) / 2;
+        anova_matrix(s, 2) = (mean2(atra_warm_ipsi(s, cluster_channels, tidx)) + mean2(atra_warm_contra(s, cluster_channels, tidx))) / 2;
+    end
+    t_bl = simple_mixed_anova(anova_matrix, [], {'stress'});
+
+
+    % Channels averaged
     figure
-    for chan = 1 : length(cluster_channels)
 
-        ch = cluster_channels(chan);
+    % Averaged cluster channels, rawpow
+    subplot(2, 2, 1)
+    pd1 = squeeze(mean(atra_cold_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd2 = squeeze(mean(atra_cold_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd3 = squeeze(mean(atra_warm_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd4 = squeeze(mean(atra_warm_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    hold on
+    plot(tf_times, pd1, '-.', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd2, '-', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd3, '-.', 'Color', 'k', 'LineWidth', 2.5)
+    plot(tf_times, pd4, '-', 'Color', 'k', 'LineWidth', 2.5)
+    hold off
+    grid on
+    xlabel('ms')
+    ylabel('\muV^2') 
+    title('alpha raw power')
 
-        pd1 = squeeze(mean(atra_z_cold_ipsi(:, ch, :), 1));
-        pd2 = squeeze(mean(atra_z_cold_contra(:, ch, :), 1));
-        pd3 = squeeze(mean(atra_z_warm_ipsi(:, ch, :), 1));
-        pd4 = squeeze(mean(atra_z_warm_contra(:, ch, :), 1));
+    % Averaged cluster channels, z-normalized
+    subplot(2, 2, 2)
+    pd1 = squeeze(mean(atra_z_cold_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd2 = squeeze(mean(atra_z_cold_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd3 = squeeze(mean(atra_z_warm_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd4 = squeeze(mean(atra_z_warm_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    hold on
+    plot(tf_times, pd1, '-.', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd2, '-', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd3, '-.', 'Color', 'k', 'LineWidth', 2.5)
+    plot(tf_times, pd4, '-', 'Color', 'k', 'LineWidth', 2.5)
+    hold off
+    grid on
+    xlabel('ms')
+    ylabel('z') 
+    title('alpha z-normalized')
+    legend({'stress ipsi', 'stress contra', 'control ipsi', 'control contra'})
 
+    % Difference wave raw
+    subplot(2, 2, 3)
+    pd1 = squeeze(mean(atra_cold_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2])) - squeeze(mean(atra_cold_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd2 = squeeze(mean(atra_warm_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2])) - squeeze(mean(atra_warm_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    hold on
+    plot(tf_times, pd1, '-', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd2, '-', 'Color', 'k', 'LineWidth', 2.5)
+    hold off
+    grid on
+    xlabel('ms')
+    ylabel('\muV^2') 
+    title('alpha raw power')
 
-        subplot(2, 3, chan)
-        plot(tf_times, [pd1, pd2, pd3, pd4])
-        title(EEG.chanlocs(ch).labels)
+    % Difference wave z-normalized
+    subplot(2, 2, 4)
+    pd1 = squeeze(mean(atra_z_cold_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2])) - squeeze(mean(atra_z_cold_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    pd2 = squeeze(mean(atra_z_warm_ipsi(:, [18, 21, 23, 27, 24, 26], :), [1, 2])) - squeeze(mean(atra_z_warm_contra(:, [18, 21, 23, 27, 24, 26], :), [1, 2]));
+    hold on
+    plot(tf_times, pd1, '-', 'Color', '#4DBEEE', 'LineWidth', 2.5)
+    plot(tf_times, pd2, '-', 'Color', 'k', 'LineWidth', 2.5)
+    hold off
+    grid on
+    xlabel('ms')
+    ylabel('z') 
+    title('alpha z-normalized')
+    legend({'stress', 'control'})
+    
 
-        if chan == 6
-            legend({'coldips', 'coldcon', 'warmips', 'warmcon'})
-        end
-    end
-
-
-end
-
-% ============================ Part 2: stats ============================================================================
-if ismember('part2', to_execute)
-
-    % Init eeglab
-    addpath(PATH_EEGLAB);
-    eeglab;
-
-    % Init fieldtrip
-    addpath(PATH_FIELDTRIP);
-    ft_defaults;
-
-    % Get tf params
-    tf_times = dlmread([PATH_TFDECOMP 'tf_times.csv']); 
-    tf_freqs = dlmread([PATH_TFDECOMP 'tf_freqs.csv']);
-
-    % Load ERSPs
-    load([PATH_TFDECOMP 'ersp_cold_ipsi']);
-    load([PATH_TFDECOMP 'ersp_cold_contra']);
-    load([PATH_TFDECOMP 'ersp_warm_ipsi']);
-    load([PATH_TFDECOMP 'ersp_warm_contra']);
-
-    % Smooth data in time
-    winlength = 50;
-    if winlength
-        ersp_cold_ipsi = movmean(ersp_cold_ipsi, winlength, 3);
-        ersp_cold_contra = movmean(ersp_cold_contra, winlength, 3);
-        ersp_warm_ipsi = movmean(ersp_warm_ipsi, winlength, 3);
-        ersp_warm_contra = movmean(ersp_warm_contra, winlength, 3);
-    end
-
-    % Get channel labels and coordinates
-    EEG = pop_loadset('filename', [num2str(subject_list(1)) '_autocleaned.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'info');
-    chanlocs = EEG.chanlocs;
-    chanlabs = {};
-    coords = [];
-    for c = 1 : numel(chanlocs)
-        chanlabs{c} = chanlocs(c).labels;
-        coords(c, :) = [chanlocs(c).X, chanlocs(c).Y, chanlocs(c).Z];
-    end
- 
-    % A sensor struct
-    sensors = struct();
-    sensors.label = chanlabs;
-    sensors.chanpos = coords;
-    sensors.elecpos = coords;
-
-    % Prepare neighbor struct
-    cfg                 = [];
-    cfg.elec            = sensors;
-    cfg.feedback        = 'no';
-    cfg.method          = 'triangulation'; 
-    neighbours          = ft_prepare_neighbours(cfg);
-
-    % A template for GA structs
-    ga_template = [];
-    ga_template.dimord = 'chan_freq_time';
-    ga_template.label = chanlabs;
-    ga_template.freq = tf_freqs;
-    ga_template.time = tf_times;
-
-    % Build condition ga
-    cfg=[];
-    cfg.keepindividual = 'yes';
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_cold_ipsi(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_cold_ipsi = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_cold_contra(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_cold_contra = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_warm_ipsi(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_warm_ipsi = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_warm_contra(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_warm_contra = ft_freqgrandaverage(cfg, D{1, :});
-
-    % Build difference ga
-    cfg=[];
-    cfg.keepindividual = 'yes';
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_cold_ipsi(s, :, :, :)) - squeeze(ersp_cold_contra(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_diff_in_cold = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_warm_ipsi(s, :, :, :)) - squeeze(ersp_warm_contra(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_diff_in_warm = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_warm_ipsi(s, :, :, :)) - squeeze(ersp_cold_ipsi(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_diff_in_ipsi = ft_freqgrandaverage(cfg, D{1, :});
-    D = {};
-    for s = 1 : length(subject_list)
-        ga_template.powspctrm = squeeze(ersp_warm_contra(s, :, :, :)) - squeeze(ersp_cold_contra(s, :, :, :));
-        D{s} = ga_template;
-    end 
-    GA_diff_in_contra = ft_freqgrandaverage(cfg, D{1, :});
-
-    % Testparams
-    testalpha  = 0.025;
-    voxelalpha  = 0.01;
-    nperm = 1000;
-
-    % Set config. Same for all tests
-    cfg = [];
-    cfg.tail             = 0; % Two sided test!
-    cfg.statistic        = 'depsamplesT';
-    cfg.alpha            = testalpha;
-    cfg.neighbours       = neighbours;
-    cfg.minnbchan        = 2;
-    cfg.method           = 'montecarlo';
-    cfg.correctm         = 'cluster';
-    cfg.clustertail      = 0;
-    cfg.clusteralpha     = voxelalpha;
-    cfg.clusterstatistic = 'maxsum';
-    cfg.numrandomization = nperm;
-    cfg.computecritval   = 'yes'; 
-    cfg.ivar             = 1;
-    cfg.uvar             = 2;
-    cfg.design           = [ones(1, numel(subject_list)), 2 * ones(1, numel(subject_list)); 1 : numel(subject_list), 1 : numel(subject_list)];
-
-    % The tests
-    [stat_interaction] = ft_freqstatistics(cfg, GA_cold_contra, GA_cold_ipsi);  
-
-
-    % Calculate effect sizes
-    adjpetasq_cold = [];
-    for ch = 1 : 32
-
-        petasq = (squeeze(stat_interaction.stat(ch, :, :)) .^ 2) ./ ((squeeze(stat_interaction.stat(ch, :, :)) .^ 2) + (numel(subject_list) - 1));
-        adj_petasq = petasq - (1 - petasq) .* (1 / (numel(subject_list) - 1));
-        adjpetasq_cold(ch, :, :) = adj_petasq;
-
-
-    end
-
-
-
-
-    %pd = GA_warm_contra;
-    %topoplot(pd, chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
-
-
-
-
-
-
-    % Repeat testalpha
-    testalpha  = 0.025;
-
-    % Set colors
-    cmap = 'jet';
-    clinecol = 'k';
-
-    % Identify significant clusters
-    clusts = struct();
-    cnt = 0;
-    stat_names = {'stat_interaction'};
-    for s = 1 : numel(stat_names)
-        stat = eval(stat_names{s});
-        if ~isempty(stat.negclusters)
-            neg_idx = find([stat.negclusters(1, :).prob] < testalpha);
-            for c = 1 : numel(neg_idx)
-                cnt = cnt + 1;
-                clusts(cnt).testlabel = stat_names{s};
-                clusts(cnt).clustnum = cnt;
-                clusts(cnt).time = stat.time;
-                clusts(cnt).freq = stat.freq;
-                clusts(cnt).polarity = -1;
-                clusts(cnt).prob = stat.negclusters(1, neg_idx(c)).prob;
-                clusts(cnt).idx = stat.negclusterslabelmat == neg_idx(c);
-                clusts(cnt).stats = clusts(cnt).idx .* stat.stat * -1;
-                clusts(cnt).chans_sig = find(logical(mean(clusts(cnt).idx, [2,3])));
-            end
-        end
-        if ~isempty(stat.posclusters)
-            pos_idx = find([stat.posclusters(1, :).prob] < testalpha);
-            for c = 1 : numel(pos_idx)
-                cnt = cnt + 1;
-                clusts(cnt).testlabel = stat_names{s};
-                clusts(cnt).clustnum = cnt;
-                clusts(cnt).time = stat.time;
-                clusts(cnt).freq = stat.freq;
-                clusts(cnt).polarity = 1;
-                clusts(cnt).prob = stat.posclusters(1, pos_idx(c)).prob;
-                clusts(cnt).idx = stat.posclusterslabelmat == pos_idx(c);
-                clusts(cnt).stats = clusts(cnt).idx .* stat.stat;
-                clusts(cnt).chans_sig = find(logical(mean(clusts(cnt).idx, [2,3])));
-            end
-        end
-    end
-
-    % Plot identified cluster
-    for cnt = 1 : numel(clusts)
-        figure('Visible', 'off'); clf;
-        subplot(2, 2, 1)
-        pd = squeeze(sum(clusts(cnt).stats, 1));
-        contourf(clusts(cnt).time, clusts(cnt).freq, pd, 40, 'linecolor','none')
-        hold on
-        contour(clusts(cnt).time, clusts(cnt).freq, logical(squeeze(mean(clusts(cnt).idx, 1))), 1, 'linecolor', clinecol, 'LineWidth', 2)
-        colormap(cmap)
-        set(gca, 'xlim', [clusts(cnt).time(1), clusts(cnt).time(end)], 'clim', [-max(abs(pd(:))), max(abs(pd(:)))], 'YScale', 'log', 'YTick', [4, 8, 12, 20, 30])
-        colorbar;
-        title(['sum t across chans, plrt: ' num2str(clusts(cnt).polarity)], 'FontSize', 10)
-        subplot(2, 2, 2)
-        pd = squeeze(mean(clusts(cnt).idx, 1));
-        contourf(clusts(cnt).time, clusts(cnt).freq, pd, 40, 'linecolor','none')
-        hold on
-        contour(clusts(cnt).time, clusts(cnt).freq, logical(squeeze(mean(clusts(cnt).idx, 1))), 1, 'linecolor', clinecol, 'LineWidth', 2)
-        colormap(cmap)
-        set(gca, 'xlim', [clusts(cnt).time(1), clusts(cnt).time(end)], 'clim', [-1, 1], 'YScale', 'log', 'YTick', [4, 8, 12, 20, 30])
-        colorbar;
-        title(['proportion chans significant'], 'FontSize', 10)
-        subplot(2, 2, 3)
-        pd = squeeze(sum(clusts(cnt).stats, [2, 3]));
-        topoplot(pd, chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
-        colormap(cmap)
-        set(gca, 'clim', [-max(abs(pd(:))), max(abs(pd(:)))])
-        colorbar;
-        title(['sum t per electrode'], 'FontSize', 10)
-        subplot(2, 2, 4)
-        pd = squeeze(mean(clusts(cnt).idx, [2, 3]));
-        topoplot(pd, chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
-        colormap(cmap)
-        set(gca, 'clim', [-1, 1])
-        colorbar;
-        title(['proportion tf-points significant'], 'FontSize', 10)
-        saveas(gcf, [PATH_PLOT 'clustnum_' num2str(clusts(cnt).clustnum) '_' clusts(cnt).testlabel '.png']); 
-    end
-
-
-
-
-
-
-end % End part2
-
-
-% Function for z norming powcubes
-function[zersp] = znorm(indata, blidx1, blidx2)
-    zdata = zeros(size(indata));
-    tmp = indata(:, blidx1 : blidx2, :);
-    tmp = reshape(tmp, [size(tmp, 1), size(tmp, 2) * size(tmp, 3)]);
-    mpow = repmat(mean(tmp, 2), [1, size(indata, 2)]);
-    stdpow = repmat(std(tmp, [], 2), [1, size(indata, 2)]);
-    for t = 1 : size(indata, 3)
-        zdata(:, :, t) = (squeeze(indata(:, :, t)) - mpow) ./ stdpow;
-    end
-    zersp = squeeze(mean(zdata, 3));
 end
